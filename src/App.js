681c1c4f49c0de57d8189ada45539ba037cee50e
devilla <dev.koold@gmail.com>
2018-11-19 16:20:23 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,37 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import axios from 'axios';
 
 class App extends Component {
+  constructor(){
+    super();
+    this.state = {
+        username: '',
+        avatar_url: '',
+        html_url : null
+    }
+  }
+  handleChange = (e) => {
+    this.setState({username: e.target.value})
+    axios.get('https://api.github.com/users/'+e.target.value).then(res=>{
+      console.log(res.data.avatar_url);
+      this.setState({avatar_url: res.data.avatar_url});
+      this.setState({html_url: res.data.html_url});
+    });
+  }
   render() {
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
+          <img src={this.state.avatar_url} className="App-logo" alt="logo" />
+          {this.state.html_url!=null && <a href={this.state.html_url}> Profile Url</a>}
+          <input
+          type="text"
+          placeholder="Enter username"
+          value={this.state.username}
+          onChange={this.handleChange}/>
+
         </header>
       </div>
     );
