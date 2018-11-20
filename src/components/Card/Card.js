577ac6b79c042c30edfa069756865ec34e937b17
devilla <dev.koold@gmail.com>
2018-11-20 17:02:29 +0530

    spinner + loaders

--- src/components/Card/Card.js
+++ src/components/Card/Card.js
@@ -1,7 +1,8 @@
 import React, { Component } from 'react';
 import './Card.css';
 import axios from 'axios';
-
+import '@material/card/dist/mdc.card.css';
+import Spinner from '../Spinner/Spinner';
 export default class Card extends Component{
   constructor(){
     super();
@@ -9,11 +10,13 @@ export default class Card extends Component{
         profile:'',
         username: '',
         avatar_url: null,
-        html_url : null
+        html_url : null,
+        loading:false
     }
   }
 handleChange = async (e) => {
     this.setState({username: e.target.value})
+    this.setState({loading:true});
     try {
       const res = await axios.get('https://api.github.com/users/'+e.target.value);
       this.setState({profile: 'found'});
@@ -22,18 +25,19 @@ handleChange = async (e) => {
     } catch (e) {
       console.log('>>>>>>>Profile doesnt exist!');
       this.setState({profile: null});
-
+      this.setState({loading:false});
     } finally {
-
+      this.setState({loading:false});
     }
   }
   render() {
     return (
       <div className="App">
         <header className="App-header">
-          <div className="Card">
+          <div className="mdc-card">
+          {this.state.profile==null && <Spinner loading={this.state.loading}/>}
           {this.state.profile!=null && this.state.avatar_url!=null && <img src={this.state.avatar_url} className="App-logo" alt="logo" />}
-          {this.state.profile!=null && this.state.html_url!=null && <div><a href={this.state.html_url} className="App-logo"> Profile Url</a></div>}
+          {this.state.profile!=null && this.state.html_url!=null && <div><a href={this.state.html_url} className="App-logo" style={{color: 'cornflowerblue'}}> Profile Url</a></div>}
           </div>
           <h3> Enter Github username to search profile</h3>
           <input
